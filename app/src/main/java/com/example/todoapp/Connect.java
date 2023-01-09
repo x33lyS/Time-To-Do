1c58804411bfc0adda64528ed075d16c402957a7
FLEJOSNE <florian.lejosne@ynov.com>
2023-01-09 12:23:26 +0100

    [ADD] firebase_connect

--- app/src/main/java/com/example/todoapp/Connect.java
+++ app/src/main/java/com/example/todoapp/Connect.java
@@ -3,6 +3,7 @@ package com.example.todoapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,15 +23,20 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
-public class Connect extends AppCompatActivity {
+public class Connect<DateTime> extends AppCompatActivity {
 
     public static final String TAG = "Connect";
     private RecyclerView recyclerView;
+    private DescriptionAdapter adapter;
+    private List<String> descriptions;
 
 
 
@@ -70,29 +76,80 @@ public class Connect extends AppCompatActivity {
         });
 
 
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        descriptions = new ArrayList<>();
+        adapter = new DescriptionAdapter(descriptions);
+        recyclerView.setAdapter(adapter);
+
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://timetodo-9a390-default-rtdb.europe-west1.firebasedatabase.app");
         DatabaseReference myRef = database.getReference("Tasks");
         // Lecture de la valeur unique à partir de la base de données
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-
                     Object description = childSnapshot.getValue();
-                    Log.v(TAG, "Value is: " + description);
+                    HashMap hashMap = (HashMap) description;
+                    Set<String> key = hashMap.keySet();
+                    for (String k : key) {
+                        // Add the data from the database to the mDescriptions list
+                        descriptions.add(k + " : " + hashMap.get(k));
+                        Log.v(TAG, "Value is: " + k + " : " + hashMap.get(k));
+                    }
                 }
-                recyclerView = findViewById(R.id.recycler_view);
+                // Notify the adapter that the data has changed and the RecyclerView should be updated
+                adapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Traitement de l'erreur ici...
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "Failed to read value.", databaseError.toException());
             }
         });
-    }
 
+
+    }
     public void show(FragmentManager supportFragmentManager, String tag) {
     }
+    public class DescriptionAdapter extends RecyclerView.Adapter<DescriptionAdapter.ViewHolder> {
+
+        private List<String> mDescriptions;
+
+        public DescriptionAdapter(List<String> descriptions) {
+            mDescriptions = descriptions;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            String description = mDescriptions.get(position);
+            holder.descriptionTextView.setText(description);
+        }
+
+        @Override
+        public int getItemCount() {
+            Log.v(TAG, "mDescriptions.size() = " + mDescriptions.size());
+            return mDescriptions.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView descriptionTextView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                descriptionTextView = itemView.findViewById(R.id.description_item);
+            }
+        }
+    }
+
 }
 
 
